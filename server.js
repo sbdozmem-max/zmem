0bcf1cae8d642fbaef2b931de48e9d79996e3e1c
sbdozmem-max <sbdozmem@gmail.com>
2026-02-14 18:03:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-const express = require('express');
-const path = require('path');
-const axios = require('axios');
-
-const app = express();
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.post('/users', async (req, res) => {
     const { name, age } = req.body;
 
     try {
-        await axios.post(https://bacet.app.n8n.cloud/webhook-test/zmeeem, {
-            name,
-            age
+        await fetch("https://bacet.app.n8n.cloud/webhook-test/zmeeem", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                age
+            })
         });
 
         res.json({ message: "تم الإرسال إلى n8n بنجاح ✅" });
 
     } catch (error) {
-        res.status(500).json({ message: "خطأ في الإرسال إلى n8n" });
+        console.error(error);
+        res.status(500).json({ message: "فشل الإرسال إلى n8n ❌" });
     }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-
 });
