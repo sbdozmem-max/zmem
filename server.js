c809390e86ee6200ad4045693b72ed881cc63f8f
sbdozmem-max <sbdozmem@gmail.com>
2026-02-14 17:55:48 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const { name, age } = req.body;
 
-    if (!name || !age) {
-        return res.status(400).json({ message: "جميع الحقول مطلوبة" });
+    try {
+        await axios.post("https://bacet.app.n8n.cloud/webhook-test/zmeeem", {
+            name,
+            age
+        });
+
+        res.json({ message: "تم الإرسال إلى n8n بنجاح ✅" });
+
+    } catch (error) {
+        res.status(500).json({ message: "خطأ في الإرسال إلى n8n" });
     }
-
-    res.json({
-        message: "تم استلام البيانات بنجاح ✅",
-        data: { name, age }
-    });
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
